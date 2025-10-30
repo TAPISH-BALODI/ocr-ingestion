2edb649f97bb556e80aeb8e6ca9a0d6cbb2ee0bb
Tapish-Evivve <tapish@gamitar.com>
2025-10-30 21:58:55 +0530

    removed manual global guard

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,8 @@
 ﻿import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { JwtAuthGuard } from './common/guards/jwt-auth.guard';
-import { TenantScopeGuard } from './auth/tenant-scope.guard';
-import { RolesGuard } from './auth/roles.guard';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { logger: ['log','error','warn','debug'] });
   app.setGlobalPrefix('v1');
-  app.useGlobalGuards(app.get(JwtAuthGuard), app.get(TenantScopeGuard), app.get(RolesGuard));
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
